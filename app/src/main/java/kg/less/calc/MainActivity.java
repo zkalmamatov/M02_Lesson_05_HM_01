1f07628674ef411edd1c4eb90cc7d3c76b29bf90
zkalmamatov <jolboldu39@gmail.com>
2024-06-23 18:25:33 +0600

    были добавлены изменения

--- app/src/main/java/kg/less/calc/MainActivity.java
+++ app/src/main/java/kg/less/calc/MainActivity.java
@@ -1,9 +1,17 @@
 package kg.less.calc;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,11 +29,30 @@ public class MainActivity extends AppCompatActivity {
     private double num2 = Double.NaN;
     private char operator;
 
+    private boolean isLiked = false;
+    private ImageButton imageButtonHeart;
 
+    private Spinner spinnerOptions;
+
+    private SeekBar seekBarMin;
+    private SeekBar seekBarMax;
+    private View line;
+
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
+
+
+
+
+
         resultTextView = findViewById(R.id.text_view);
 
         setupButtonClickListener(R.id.button0, "0");
@@ -51,7 +78,91 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonDiv).setOnClickListener(v -> setOperator('/'));
         findViewById(R.id.buttonEqual).setOnClickListener(v -> calculateResult());
 
+
+        imageButtonHeart = findViewById(R.id.heart);
+
+        imageButtonHeart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isLiked) {
+                    imageButtonHeart.setImageResource(R.drawable.heart_line);
+                } else {
+                    imageButtonHeart.setImageResource(R.drawable.heart_filled);
+                }
+                isLiked = !isLiked;
+            }
+        });
+
+
+        Button nextButton = findViewById(R.id.next_button);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        spinnerOptions = findViewById(R.id.spiner_options);
+
+        spinnerOptions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedOptions = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, "Выбрано: " + selectedOptions, Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+
+        seekBarMin = findViewById(R.id.seekbar_min);
+        seekBarMax = findViewById(R.id.seekbar_max);
+        line = findViewById(R.id.line);
+
+        seekBarMin.setProgress(20);
+        seekBarMax.setProgress(80);
+
+        seekBarMin.setOnSeekBarChangeListener(seekBarChangeListener);
+        seekBarMax.setOnSeekBarChangeListener(seekBarChangeListener);
+
+
     }
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (seekBar.getId() == R.id.seekbar_min) {
+                updateLinePosition();
+            } else if (seekBar.getId() == R.id.seekbar_max) {
+                updateLinePosition();
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private void updateLinePosition() {
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) line.getLayoutParams();
+        layoutParams.leftMargin = seekBarMin.getThumb().getBounds().centerX();
+        layoutParams.rightMargin = seekBarMax.getWidth() - seekBarMax.getThumb().getBounds().centerX();
+        line.setLayoutParams(layoutParams);
+    }
+
+public void goToNextActivity (View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
+}
+
+
 
     private void setupButtonClickListener(int buttonId, String text) {
         Button button = findViewById(buttonId);
@@ -160,15 +271,10 @@ public class MainActivity extends AppCompatActivity {
         resultTextView.setText("0");
     }
 
-//    public void oneClick(View view) {
-//        String textButton = ((MaterialButton) view).getText().toString();
-//        if (textButton.equals("AC")) {
-//            textView.setText("0");
-//        } else if (textView.getText().toString().equals("0")) {
-//            textView.setText(textButton);
-//        } else {
-//            textView.append(textButton);
-//        }
-//
-//    }
+
+    public MainActivity(ImageButton imageButtonHeart) {
+        this.imageButtonHeart = imageButtonHeart;
+    }
+
+
 }
